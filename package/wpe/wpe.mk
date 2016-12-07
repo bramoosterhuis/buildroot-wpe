533a40de88c4637e165c7f7a2c3547d8859584b5
haseenamol@tataelxsi.co.in <haseenamol@tataelxsi.co.in>
2016-12-07 12:59:17 +0000

    wpe patch removed

--- package/wpe/wpe.mk
+++ package/wpe/wpe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPE_VERSION = 622e996fc528212bd0453a076571261d28328d34
+WPE_VERSION = e4d2687f616e184f4dc9fe792f0be320a660eab2
 WPE_SITE = $(call github,Metrological,WebKitForWayland,$(WPE_VERSION))
 
 WPE_INSTALL_STAGING = YES
